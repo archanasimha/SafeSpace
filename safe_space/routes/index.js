6f21ce565c21633dbd0e6c87ee29579d51e46600
nicolebohra <nicolebohra@gmail.com>
2016-10-16 00:42:03 -0700

    added errors page

--- safe_space/routes/index.js
+++ safe_space/routes/index.js
@@ -3,19 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('main_page', { title: 'SafeSpace' });
+  res.render('main_page');
+});
+router.get('/home', function(req, res, next) {
+  res.render('home');
 });
 
 /* GET home page. */
 router.get('/events', function(req, res, next) {
-  res.render('events', { title: 'Events' });
+  res.render('events');
 });
 router.get('/events/recent', function(req, res, next) {
-  res.render('recentevents', { title: 'Recent Events' });
+  res.render('recentevents');
 });
 
 router.get('/advice', function(req, res, next) {
-  res.render('advice', { title: 'Advice' });
+  res.render('advice');
 });
 
 
