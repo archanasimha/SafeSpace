3d5a1e72fc2740a7ecd46e54b7a5d85fb6ed598a
nicolebohra <nicolebohra@gmail.com>
2016-10-16 00:18:36 -0700

    created new pages

--- safe_space/routes/index.js
+++ safe_space/routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SafeSpace' });
 });
+
+/* GET home page. */
+router.get('/events', function(req, res, next) {
+  res.render('events', { title: 'Events' });
+});
+router.get('/events/recent', function(req, res, next) {
+  res.render('recentevents', { title: 'Recent Events' });
+});
+
+router.get('/advice', function(req, res, next) {
+  res.render('advice', { title: 'Advice' });
+});
+
 
 module.exports = router;
